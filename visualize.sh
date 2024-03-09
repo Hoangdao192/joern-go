c28e79e7c80679ae09f2e27744db86ed7c6a614f
Nguyễn Đăng Hoàng Đạo <nguyendanghoangdao2002@gmail.com>
2024-03-09 22:16:06 +0700

    visualize graph to neo4j

--- visualize.sh
+++ visualize.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-inputCpg=$0
-outputDir=$1
+inputCpg=$1
+
+echo "Input cpg: $inputCpg"
+
+outputDir=/tmp/visualize_$(date +%s)
+
+echo "$outputDir"
 
 # Generate import file
-joern-export --repr=all --format=neo4jcsv --out=outputDir inputCpg
+joern-export --repr=all --format=neo4jcsv --out="$outputDir" "$inputCpg"
 
 # Remove old import file
 docker exec neo4j bash -c "rm -rf /var/lib/neo4j/import/*"
 # Remove old graph
 docker exec neo4j bash -c "cypher-shell -u neo4j -p 12345678 -d neo4j \"MATCH (n) DETACH DELETE n\""
 
-docker cp outputDir/. neo4j:/var/lib/neo4j/import
+docker cp "$outputDir"/. neo4j:/var/lib/neo4j/import
 
-find /var/lib/neo4j/import/ -name 'nodes_*_cypher.csv' -exec cypher-shell -u neo4j -p 12345678 -d neo4j --file {} \;
-find /var/lib/neo4j/import/ -name 'edges_*_cypher.csv' -exec cypher-shell -u neo4j -p 12345678 -d neo4j --file {} \;
+rm -rf "$outputDir"
+
+docker exec neo4j bash -c "find /var/lib/neo4j/import/ -name 'nodes_*_cypher.csv' -exec cypher-shell -u neo4j -p 12345678 -d neo4j --file {} \;"
+docker exec neo4j bash -c "find /var/lib/neo4j/import/ -name 'edges_*_cypher.csv' -exec cypher-shell -u neo4j -p 12345678 -d neo4j --file {} \;"
 
 
 
